142ff5f397762a19e27a4f33237574575f432dea
JimmyGee123 <jseignior@live.com.au>
2022-04-20 15:28:16 +1000

    js

--- js/app.js
+++ js/app.js
@@ -147,7 +147,7 @@ async function fetchWithRetry(url)  {
   });
 }
 
-async function generateImages(editionsList){
+async function generateImages(edition){
   //let url = 'https://opensea.mypinata.cloud/ipfs/bafybeic7euhz6awhrg4ill6zvvvryg5cg6mjirqhjbhyvhznlv2uvrbq6a/' + editionsList + '.json';
   //let obj = null;
  // try {
@@ -155,13 +155,13 @@ async function generateImages(editionsList){
   //} catch(e) {
   //    console.log('error');
   //}
-  console.log(editionsList);
+  console.log(edition,index);
   //document.getElementById('owner-editions').insertAdjacentHTML('beforeend', obj.image + '\n');
   if (Object.is(editionsList.length - 1)) {
     // execute last item logic
-    document.getElementById('owner-editions').insertAdjacentHTML('beforeend', '#'+editionsList);
+    document.getElementById('owner-editions').insertAdjacentHTML('beforeend', '#'+edition);
     //console.log(`Last callback call at index ${key} with value ${val}` ); 
   }else{
-    document.getElementById('owner-editions').insertAdjacentHTML('beforeend', '#'+editionsList+',');
+    document.getElementById('owner-editions').insertAdjacentHTML('beforeend', '#'+edition+',');
   }
 }
